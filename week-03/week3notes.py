8da3c0b3fe58999ee4ac8953e7a94b5cd2ea2f90
Louis Liss <lliss@mit.edu>
2018-02-21 11:03:16 -0500

    notes from class 2/22

--- week-03/week3notes.py
+++ week-03/week3notes.py
@@ -1,9 +1,9 @@
 x = range(10)
-
 for i in x:
     if ((i*2) > 5) :
         break
     print(i)
+
 
 my_list = ['This','is','Python']
 for i in my_list:
@@ -33,3 +33,63 @@ c = []
 for i,j in zip(a, b):
     c.append(i + j)
 print(c)
+
+##PANDAS Workshop
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame()
+print(df)
+
+df['name'] = ['Joey', 'Jeremy', 'Jenny']
+
+df.assign(age = [28, 37, 27])
+
+import os
+os.chdir('week-03')
+
+df = pd.read_csv('data/skyhook_2017-07.csv', sep=',')
+
+df.head()
+df.shape
+df.columns
+df['cat_name'].unique()
+df.cat_name
+#we're only intrested in hour 158
+onefiveeight = df[df['hour'] == 158]
+onefiveeight.shape
+#we're only interested in rows with more than 50 GPS pings during hour 158
+ofesubset = df[(df['hour'] == 158) & (df['count'] > 50)]
+
+bastille = df[df['date'] == '2017-07-14']
+bastille.head()
+
+#greater than average cells--creating a mask
+bastille_lovers = bastille[bastille['count'] > bastille['count'].mean()]
+bastille_lovers['count'].describe()
+
+import matplotlib
+%matplotlib inline
+
+df.groupby('date')['count'].describe()
+df['hour'].unique()
+jul_second = df[df['date'] == '2017-07-02']
+jul_second.groupby('hour')['count'].sum().plot()
+
+df['date_new'] = pd.to_datetime(df['date'],format='%Y-%m-%d')
+df['date_new'].unique()
+
+df[df['date_new'] == '2017-07-02']['weekday']
+
+df['weekday'] = df['date_new'].apply(lambda x: x.weekday()+1)
+df['weekday'].replace(7, 0, inplace = True)
+
+
+'''for i in range(0, 168, 24):
+    j = range()
+    df.drop(df[df['weekday'] == (i/24) &
+    (
+    (df['hour'] < j | df ['hour'] > j + 18)
+    )
+    ])''' 
